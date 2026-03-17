5e45882b3456cf8ee6d15df0a232845de79e72f0
wuhanstudio <wuhanstudios@gmail.com>
2026-03-17 14:41:08 +0000

    [plot] Add scroller

--- main_plot.py
+++ main_plot.py
@@ -4,6 +4,7 @@ from loguru import logger
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 
 from detector import EdgeDetector
 
@@ -44,16 +45,85 @@ def plot_edge_detection(dataframe, noise_level=50, state_threshold=15):
         )
 
     # Plot steady states with main
-    ax = dataframe.plot()
-    if not steady_states.empty:
-        steady_states.plot(style="o", ax=ax)
-        for _, tran in transients.iterrows():
-            plt.axvline(x=tran["start time"], color='r', linestyle='--', label='Start Time')
-            # plt.axvline(x=tran["end time"], color='g', linestyle='--', label='End Time')
+    # ax = dataframe.plot()
+    # if not steady_states.empty:
+    #     steady_states.plot(style="o", ax=ax)
+    #     for _, tran in transients.iterrows():
+    #         plt.axvline(x=tran["start time"], color='r', linestyle='--', label='Start Time')
+    #         # plt.axvline(x=tran["end time"], color='g', linestyle='--', label='End Time')
 
-    plt.legend(["Measurement", "Steady states"])
-    plt.ylabel("Power (W)")
-    plt.xlabel("Time")
+    # plt.legend(["Measurement", "Steady states"])
+    # plt.ylabel("Power (W)")
+    # plt.xlabel("Time")
+    # plt.show()
+
+    # --- Interactive plotting ---
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.25)
+
+    # Fixed y-axis
+    y_min = dataframe.min().item()
+    y_max = dataframe.max().item()
+    ax.set_ylim(y_min, y_max)
+
+    window_size = 1200
+
+    # Initial plot window
+    x0 = dataframe.index[:window_size]
+    y0 = dataframe.iloc[:window_size].values
+    line_main, = ax.plot(x0, y0, color='blue')
+
+    # Steady states circles
+    line_states = None
+    if not steady_states.empty:
+        mask = (steady_states.index >= x0[0]) & (steady_states.index <= x0[-1])
+        line_states, = ax.plot(
+            steady_states.index[mask],
+            steady_states["active average"][mask],
+            'o',
+            # label="Steady states",
+            color='orange'
+        )
+
+    # Transient start lines
+    for i, (_, tran) in enumerate(transients.iterrows()):
+        ax.axvline(x=tran["start time"], color='r', linestyle='--')
+
+    ax.set_xlim(x0[0], x0[-1])
+    ax.set_ylabel("Power (W)")
+    ax.set_xlabel("Time")
+    ax.legend()
+
+    # Slider
+    ax_slider = plt.axes([0.2, 0.1, 0.6, 0.03])
+    slider = Slider(
+        ax_slider,
+        "Start",
+        0,
+        max(0, len(dataframe) - window_size),
+        valinit=0,
+        valstep=None  # smooth continuous sliding
+    )
+
+    # Update function
+    def update(val):
+        start = int(slider.val)
+        end = start + window_size
+
+        x = dataframe.index[start:end]
+        y = dataframe.iloc[start:end].values
+
+        line_main.set_data(x, y)
+        ax.set_xlim(x[0], x[-1])
+
+        # Update steady states circles
+        if line_states is not None:
+            mask = (steady_states.index >= x[0]) & (steady_states.index <= x[-1])
+            line_states.set_data(steady_states.index[mask], steady_states["active average"][mask])
+
+        fig.canvas.draw_idle()
+
+    slider.on_changed(update)
     plt.show()
 
     return transients, steady_states
